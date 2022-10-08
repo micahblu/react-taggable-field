92c13d935718562bb8abc4442754a75ab8b23bf9
Micah Blu <micahblu@gmail.com>
2022-10-08 15:13:41 -0700

    Adds esbuild plugin to remove node_modules

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,5 +1,6 @@
 const esbuild = require('esbuild')
 const InlineCSSPlugin = require('esbuild-plugin-inline-css')
+const { nodeExternalsPlugin } = require('esbuild-node-externals')
 
 esbuild.build({
   entryPoints: ['src/react-taggable-field/react-taggable-field.jsx'],
@@ -8,6 +9,7 @@ esbuild.build({
 	format: 'esm',
 	target: 'chrome79',
 	plugins: [
-		InlineCSSPlugin()
+		InlineCSSPlugin(),
+		nodeExternalsPlugin()
 	]
 }).catch(() => process.exit(1))
